14f65c26ebf25c35a57323bbfa56f2e9922c994c
enzo-nabert <enzo.nabert@etu.umontpellier.fr>
2021-05-12 15:20:12 +0200

    test test test

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import Head from 'next/head'
 import Link from 'next/link'
 
+import { ApolloClient, createHttpLink, gql, InMemoryCache } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
 export default function Home({ posts }) {
-  window
   return (
    <div>
      <Head>
@@ -22,10 +24,35 @@ export default function Home({ posts }) {
 }
 
 export async function getStaticProps(){
-  let posts = await fetch("http://jsonplaceholder.typicode.com/posts?_limit=5")
-  .then(r => r.json());
+  const httpLink = createHttpLink({
+    uri: `https://graphql.datocms.com/`
+  });
 
+  const authLink = setContext((_, { headers }) => {
+    return {
+      headers: {
+        ...headers,
+        authorization: `Bearer ${process.env.NEXT_DATOCMS_API_TOKEN}`
+      }
+    };
+  });
+
+  const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache()
+  });
+
+  const { data } = await client.query({
+    query: gql`
+      allModelmultiples {
+        id
+        titre
+        float
+        integer
+      }
+    `
+  });
   return {
-    props : {posts}
-  }
+    props: { data }
+  };
 }
